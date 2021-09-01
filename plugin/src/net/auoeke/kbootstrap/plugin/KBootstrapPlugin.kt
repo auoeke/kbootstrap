fd315831273a9d4e7fd8bcbb47a3a8499cab221b
auoeke <username11681@gmail.com>
2021-09-01 04:21:09 +0100

    add KBootstrap dependency during evaluation

--- plugin/src/net/auoeke/kbootstrap/plugin/KBootstrapPlugin.kt
+++ plugin/src/net/auoeke/kbootstrap/plugin/KBootstrapPlugin.kt
@@ -10,7 +10,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 class KBootstrapPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         project.plugins.apply("org.jetbrains.kotlin.jvm")
-
         val java = project.extensions.getByType(JavaPluginExtension::class.java)
 
         project.tasks.withType(KotlinCompile::class.java) {compileKotlin ->
@@ -19,12 +18,11 @@ class KBootstrapPlugin : Plugin<Project> {
 
         val extension = KBootstrapExtension()
         project.extensions.add("kbootstrap", extension)
+        project.dependencies.add("include", project.dependencies.add("modApi", "net.auoeke:kbootstrap:latest.release"))
 
         val task = project.tasks.create("kbootstrapMarker", GenerateMarker::class.java, java.sourceSets.getByName("main"), extension)
 
         project.afterEvaluate {
-            project.dependencies.add("include", project.dependencies.add("modApi", "net.auoeke:kbootstrap:latest.release"))
-
             extension.modules.forEach {
                 when (it) {
                     "coroutines" -> with(project.dependencies) {
